dfe0c56f544460257c9cd17cf479c8eeacb8bc07
INDERJEETBHADU <bishnoiinderjeet580@gmail.com>
2024-05-03 15:05:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './App2.css';
+import './App3.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import Footer from './components/Footer';
