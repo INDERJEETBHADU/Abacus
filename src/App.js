fb90400a3c63691a6a2ae0c40d2b15085961555a
Ritik <rvjaat25@gmail.com>
2024-05-03 15:28:02 +0530

    container-added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App3.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import Footer from './components/Footer';
+import Services from './components/Services';
 
 
 function App() {
   return (
     <div className="App">
-      <Footer />
+      <Services />
+      {/* <Footer /> */}
     </div>
   );
 }
