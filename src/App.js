f2df1d2497ea6a98e004529cb9e8ccc96fdbe164
vishalmanglaw453 <vishalmanglaw@gmail.com>
2024-05-03 14:45:23 +0530

    testing folder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-
+<Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h2 className=' text-black'> hello - inderjeet</h2>
+       
+    </div>
+  )
+}
+
+export default Footer
